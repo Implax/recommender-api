fcfa5559defd7d56d8145fb37be716281541d0a3
Implax <juniourclottey@gmail.com>
2023-05-06 12:06:22 +0000

    yes

--- course_pred_php.py
+++ course_pred_php.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
